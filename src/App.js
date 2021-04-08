c01f3920640e6c828a0882bda90118a2232e2e04
Justin Coleman <justinvcoleman@gmail.com>
2021-04-08 13:28:09 -0400

    Added React Router support

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './containers/Home/Home';
 import About from './containers/About/About';
 import Labs from './containers/Labs/Labs';
 
 import './assets/styles/main.scss';
-// import './App.css';
-
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      view: <Home />,
       activePage: 'home'
     }
   }
-  
+
+  componentDidMount() {
+    this.onRouteChange(window.location.pathname.match(/([0-9a-z]+)/)[0]);
+  }
+
   onRouteChange = (route) => {
     if (route === 'home') {
       this.setState({activePage: 'home'});
-      this.setState({view: <Home />});
     } else if (route === 'about') {
       this.setState({activePage: 'about'});
-      this.setState({view: <About />});
     } else if (route === 'labs') {
       this.setState({activePage: 'labs'});
-      this.setState({view: <Labs />});
     }
-
-    // this.setState({route: route})
   }
 
   render() {
-    const { view, activePage } = this.state;
+    const { activePage } = this.state;
 
-    console.log("page:", activePage);
-
-    // if (route === 'home') {
-    //   view = <Home />;
-    // } else if (route === 'about') {
-    //   view = <About />;
-    // } else if (route === 'labs') {
-    //   view = <Labs />;
-    // }
-    
     return (
-      <div>
-        <Nav onRouteChange={this.onRouteChange} activePage={activePage} />
-        {view}
-      </div>
+      <Router>
+        <div>
+          <Nav onRouteChange={this.onRouteChange} activePage={activePage} />
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/labs">
+              <Labs />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,32 +1,34 @@
-import React from 'react';  
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = ({ onRouteChange, activePage }) => {
-    console.log("activePage", activePage);
-
     return (
     <nav className="menu">
         <ul>
-            <li className="menu__item">
-                <button 
-                    onClick={() => onRouteChange('home')} 
-                    title="Home" 
+          <li className="menu__item">
+            <Link to="/">
+                <button
+                    onClick={() => onRouteChange('home')}
                     className={`menu__link ${activePage === 'home' ? 'menu__link--selected' : ''}`}
                 >Home</button>
-            </li>
-            <li className="menu__item">
-                <button 
-                    onClick={() => onRouteChange('about')} 
-                    title="About" 
+            </Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/about">
+                <button
+                    onClick={() => onRouteChange('about')}
                     className={`menu__link ${activePage === 'about' ? 'menu__link--selected' : ''}`}
                 >About</button>
-            </li>
-            <li className="menu__item">
-                <button 
-                    onClick={() => onRouteChange('labs')} 
-                    title="Labs" 
+            </Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/labs">
+                <button
+                    onClick={() => onRouteChange('labs')}
                     className={`menu__link ${activePage === 'labs' ? 'menu__link--selected' : ''}`}
                 >Labs</button>
-            </li>
+            </Link>
+          </li>
         </ul>
     </nav>
     );
